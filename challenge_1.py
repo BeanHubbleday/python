bd7e6558bd103d04c211071492f5a73355aeeaf1
beanhubbleday <bean@hubbleday.co.uk>
2019-04-03 12:37:13 +0100

    Made code more readable

--- challenge_1.py
+++ challenge_1.py
@@ -1,3 +1,4 @@
 # Challenge 1
+
 enter=input("Why did the chicken cross the road?")
 print("To get to the other side...")
--- challenge_2.py
+++ challenge_2.py
@@ -1,3 +1,4 @@
 # Challenge 2
+
 name = input("What's your name? Type it here: ")
 print("Hello",name)
