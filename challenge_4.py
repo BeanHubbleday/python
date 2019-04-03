346cd9eca54646c3bbf8508dc2ec3c8b3f042508
beanhubbleday <bean@hubbleday.co.uk>
2019-04-03 12:47:36 +0100

    Completed the extension and laid out the IF statement, now to do the task.

---
+++ challenge_4.py
@@ -0,0 +1,9 @@
+# Challenge 4
+
+option=input("Do you need to know the speed you need to travel at to arrive at your destination in the required amount of time OR know the distance travelled currently? Enter `Speed` OR `Distance`: ")
+if option == "Speed"or"speed"or"SPEED":
+    distance = int(input("How far, in miles, do you need to travel? Enter it here: "))
+    time = int(input("How long do you have to get there, in minutes? Enter it here: "))
+    conversion=0.0166667*time
+    speed=distance/conversion
+    print("You'll need to do",speed,"mph!")
