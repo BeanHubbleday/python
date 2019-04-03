fc64ece06363ea91e59483352deac70fede72cf3
beanhubbleday <bean@hubbleday.co.uk>
2019-04-03 12:34:47 +0100

    Added if statement to incoportae extension task and completed extension task.

--- challenge_3.py
+++ challenge_3.py
@@ -1,6 +1,16 @@
 # Challenge 3
 
-width=int(input("Enter the width of your rectangle: "))
-length=int(input("Enter the height of your rectangle: "))
-area=width*length
-print("The area of your rectangle is:",area)
+option = input("Do you want to find the area of a rectangle or the volume of a cuboid? Enter `Volume` OR `Area` Here: ")
+if option == "Area"or"area"or"AREA":
+    width=int(input("Enter the width of your rectangle: "))
+    length=int(input("Enter the length of your rectangle: "))
+    area=width*length
+    print("The area of your rectangle is:",area)
+elif option == "Volume"or"volume"or"VOLUME":
+    width=int(input("Enter the width of your rectangle: "))
+    length=int(input("Enter the length of your rectangle: "))
+    height=int(input("Enter the height of your rectangle: "))
+    volume=width*length*height
+    print("The volume of your cuboid is:",volume)
+else:
+    print("Incorrect input, try again!")
