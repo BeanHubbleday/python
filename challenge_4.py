0130f35892f32c7c2f4740fa15fd5097db5d9f49
beanhubbleday <bean@hubbleday.co.uk>
2019-04-03 13:02:07 +0100

    Fixed IF statement by using INT inputs instead of words...

--- challenge_4.py
+++ challenge_4.py
@@ -1,14 +1,14 @@
 # Challenge 4
 
-option=input("Do you need to know the speed you need to travel at to arrive at your destination in the required amount of time OR know the distance travelled currently? Enter `Speed` OR `Distance`: ")
-if option == "Speed"or"speed"or"SPEED":
+option = int(input("Do you need to know the speed you need to travel at to arrive at your destination in the required amount of time OR know the distance travelled currently? Enter 1 for Speed OR 2 for Distance: "))
+if option == 1:
     distance = int(input("How far, in miles, do you need to travel? Enter it here: "))
     time = int(input("How long do you have to get there, in minutes? Enter it here: "))
     conversion=0.0166667*time
     speed=distance/conversion
     print("You'll need to do",speed,"mph!")
-elif option == "Distance"or"distance"or"DISTANCE":
-    currentSpeed = int(input("What speed are you currently travelling at? Enter it here: "))
+elif option == 2:
+    currentSpeed = int(input("What speed are you currently travelling at? Enter it here, in mph: "))
     currentDriveTime = int(input("How long have you been driving for, in minutes? Enter it here: "))
     conversion=0.0166667*currentDriveTime
     distance=currentSpeed*currentDriveTime
