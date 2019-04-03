d1faa3bfada07e063f3e44527f439486cd3d785e
beanhubbleday <bean@hubbleday.co.uk>
2019-04-03 12:21:36 +0100

    Completed Challenge 3

---
+++ challenge_3.py
@@ -0,0 +1,6 @@
+# Challenge 3
+
+width=int(input("Enter the width of your rectangle: "))
+length=int(input("Enter the height of your rectangle: "))
+area=width*length
+print("The area of your rectangle is:",area)
