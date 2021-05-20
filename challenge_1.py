b45847ed0b4fed62f300888ded1331ad4fde6dd2
Jack Hubbleday <38787899+BeanHubbleday@users.noreply.github.com>
2021-05-21 00:21:27 +0100

    Completed 1 & 5

--- challenge_1.py
+++ challenge_1.py
@@ -1,4 +1,4 @@
 # Challenge 1
 
 enter=input("Why did the chicken cross the road?")
-print("To get to the other side...")
+print('To get to the other side...')
--- challenge_5.py
+++ challenge_5.py
@@ -1,7 +1,12 @@
 # Challenge 5
 
 import datetime
-dob=input("Enter your date of birth: ")
+from datetime import timedelta
+doby=int(input("Enter your year of birth (YYYY): "))
+dobm=int(input("Enter your month of birth (MM): "))
+dobd=int(input("Enter your day of birth (DD): "))
+dobdd = datetime.date(doby, dobm, dobd)
 today = datetime.date.today()
-diff = dobdd - today
-print(diff.days)
+diff = today - dobdd
+secs = diff * 86400
+print("You have been alive for", diff.days, "days, which equates to", secs, "seconds!")
